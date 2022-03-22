2150db9ada4f188f04c85c3f24d94971a7336cfa
KhangNguyenAIA <khang.ng.aia@gmail.com>
2022-03-22 11:19:17 +0700

    remove ls in main

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from starter.ml.model import inference, process_data
 import os
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     print("Running DVC")
-    os.system("ls")
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
